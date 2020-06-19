12209efa893468f4340f78e876ba39b21e856dd5
Mariano Koldobsky <mkoldobsky@gmail.com>
2020-06-18 23:08:37 -0300

    Zipcode fragment

---
+++ app/src/main/java/com/inconsciente/colectiv/ui/fragments/ZipcodeFragment.kt
@@ -0,0 +1,41 @@
+package com.inconsciente.colectiv.ui.fragments
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.inconsciente.colectiv.R
+import com.inconsciente.colectiv.ui.NavigationHost
+import kotlinx.android.synthetic.main.zipcode_fragment.*
+import kotlinx.android.synthetic.main.zipcode_fragment.view.*
+
+class ZipcodeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment.
+        val view = inflater.inflate(R.layout.zipcode_fragment, container, false)
+
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isValidZipcode(zipcode_edit_text.text!!)) {
+                zipcode_text_input.error = getString(R.string.zipcode_error)
+            } else {
+                // Clear the error.
+                zipcode_text_input.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(FirstFragment(), false)
+            }
+        }
+
+
+        return view
+    }
+
+    private fun isValidZipcode(zipcodeText: Editable?): Boolean {
+            return zipcodeText?.length == 4
+    }
+
+}
