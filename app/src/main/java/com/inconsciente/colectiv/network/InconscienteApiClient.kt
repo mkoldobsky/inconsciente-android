6439fff6168b7327388089f207bbfbb8b4fd51cb
Mariano Koldobsky <mkoldobsky@gmail.com>
2020-06-23 15:52:30 -0300

    Area validation and navigation

--- app/src/main/java/com/inconsciente/colectiv/network/InconscienteApiClient.kt
+++ app/src/main/java/com/inconsciente/colectiv/network/InconscienteApiClient.kt
@@ -5,10 +5,14 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Response
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 //private const val BASE_URL = "https://inconsciente-api.herokuapp.com"
 private const val BASE_URL = "https://inconsciente-api.azurewebsites.net"
@@ -21,13 +25,17 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    //.addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface InconscienteApiClient {
-    @GET("message")
+    @GET("/message")
     fun getMessagePropertiesAsync():
             Deferred<List<MessageProperty>>
+    @GET("/zipcode/{zipcode}/area")
+    suspend fun getAreaByZipcode(@Path("zipcode") zipcode : String) : Response<AreaProperty>
+
 }
 
 
--- app/src/main/java/com/inconsciente/colectiv/ui/fragments/ZipcodeFragment.kt
+++ app/src/main/java/com/inconsciente/colectiv/ui/fragments/ZipcodeFragment.kt
@@ -5,11 +5,19 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.inconsciente.colectiv.R
+import com.inconsciente.colectiv.network.InconscienteApi
 import com.inconsciente.colectiv.ui.NavigationHost
 import kotlinx.android.synthetic.main.zipcode_fragment.*
 import kotlinx.android.synthetic.main.zipcode_fragment.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import timber.log.Timber
 
 class ZipcodeFragment : Fragment() {
     override fun onCreateView(
@@ -18,14 +26,49 @@ class ZipcodeFragment : Fragment() {
         // Inflate the layout for this fragment.
         val view = inflater.inflate(R.layout.zipcode_fragment, container, false)
 
-        view.next_button.setOnClickListener {
-            if (!isValidZipcode(zipcode_edit_text.text!!)) {
+        val validateButton = view.validate_button
+        val nextButton = view.next_button
+        val messageTextView = view.message_text_view
+
+        nextButton.setOnClickListener {
+
+            // Navigate to the next Fragment.
+            (activity as NavigationHost).navigateTo(MessageFragment(), false)
+        }
+        validateButton.setOnClickListener {
+            val zipcode = zipcode_edit_text.text
+            if (!isValidZipcode(zipcode!!)) {
                 zipcode_text_input.error = getString(R.string.zipcode_error)
             } else {
-                // Clear the error.
                 zipcode_text_input.error = null
-                // Navigate to the next Fragment.
-                (activity as NavigationHost).navigateTo(MessageFragment(), false)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    val response =
+                        InconscienteApi.retrofitService.getAreaByZipcode(zipcode.toString())
+                    withContext(Dispatchers.Main) {
+                        var message = "Algo salió mal!"
+                        try {
+                            if (response.isSuccessful) {
+                                validateButton.visibility = View.INVISIBLE
+                                nextButton.visibility = View.VISIBLE
+
+                                message = "area ${response.body()?.name}"
+                                messageTextView.text = getString(R.string.congrats_message)
+                                messageTextView.visibility = View.VISIBLE
+                                Timber.i(message)
+                                Toast.makeText(view.context, message, Toast.LENGTH_LONG)
+                            } else {
+                                messageTextView.text = getString(R.string.no_area_message)
+                                messageTextView.visibility = View.VISIBLE
+                            }
+                        } catch (e: HttpException) {
+                            message = "Exception ${e.message}"
+                        }
+                        Toast.makeText(view.context, message, Toast.LENGTH_LONG)
+                    }
+                }
+
+
             }
         }
 
@@ -34,7 +77,7 @@ class ZipcodeFragment : Fragment() {
     }
 
     private fun isValidZipcode(zipcodeText: Editable?): Boolean {
-            return zipcodeText?.length == 4
+        return zipcodeText?.length == 4
     }
 
 }
